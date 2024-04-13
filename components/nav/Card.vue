b691e63246db3a76e7334373d5812a6dc1c219bc
liunanji <lnjasdf@qq.com>
2024-04-14 00:30:56 +0800

    调整导航卡片的布局

--- components/nav/Card.vue
+++ components/nav/Card.vue
@@ -3,14 +3,22 @@ import { type ITabDetails } from "@/interface/nav";
 const props = defineProps<{
   data: ITabDetails;
 }>();
+const iconSize = ref(40);
 </script>
 
 <template>
-  <div class="flex flex-wrap bg-red-100">
-    <div><NuxtImg :src="data.icon.trim().length > 0 ? data.icon : '/icons/default.svg'" width="20" height="20"/></div>
-    <div>
-      <h4>{{ data.title }}</h4>
-      <p>{{ data.description }}</p>
+  <div class="grid grid-cols-[auto_1fr]  bg-red-100">
+    <div class="grid place-items-center">
+      <NuxtImg
+        :src="data.icon.trim().length > 0 ? data.icon : '/icons/default.svg'"
+        :width="iconSize"
+        :height="iconSize"
+        class=""
+      />
+    </div>
+    <div class="truncate">
+      <h4 class="truncate">{{ data.title }}</h4>
+      <p class="truncate">{{ data.description }}</p>
     </div>
   </div>
 </template>
