c83da5e1ee3d2682b3f42e297859e0c0b689fc12
liunanji <lnjasdf@qq.com>
2024-04-16 16:19:41 +0800

    添加站点详情页

---
+++ pages/sites/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute();
+const id = route.params.id
+</script>
+
+<template>
+  <div>{{ id }} {{ $route.params.id }}</div>
+</template>
+
+<style lang="scss" module></style>
